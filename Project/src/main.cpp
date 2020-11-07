6828bee2241ad3381cb8c44b84ac09fd27029d89
Florian Brenot <Ziink4@users.noreply.github.com>
2020-11-07 18:24:28 +0100

    Add main project

---
+++ Project/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 7, 2020
+ *      Author: Florian
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
